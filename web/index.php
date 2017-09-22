3b1a7f4c671ea92975056e423914dd000475e7f6
Senthilprabu Ponnusamy <p.senthilprabu@gmail.com>
2017-09-22 13:59:36 -0400

    Adding web files.

---
+++ web/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* @file
+* Simple hello heroku application.
+*/
+?>
+<?php date_default_timezone_set('America/New_York');?>
+<HTML>
+  <HEAD>
+    <TITLE>Hello Heroku</TITLE>
+  </HEAD>
+  <BODY>
+    <CENTER><H1>Hello Heroku!</H1></CENTER>
+    <CENTER><H3><?php print date("F j, Y, g:i a"); ?></H3></CENTER>
+    <CENTER><H5>Created By: <a href="http://www.senthilprabu.in">Senthilprabu Ponnusamy</a></H5></CENTER>
+  </BODY>
+</HTML>
